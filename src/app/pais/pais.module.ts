ed9a0bec27e2f9c42ace7cea2e941fa50a070608
coazit <coazit@gmail.com>
2021-02-28 17:32:30 -0500

    Llenar Tabla de Paises

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -2,12 +2,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 //Propias
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+
 
 
 
@@ -28,6 +30,7 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule
   ]
 })
 export class PaisModule { }
