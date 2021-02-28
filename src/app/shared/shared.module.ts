5f1a323454c3bbb95c306dfc41c5c47411e198db
coazit <coazit@gmail.com>
2021-02-28 01:55:18 -0500

    Component Sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
-  declarations: [SidebarComponent],
-  exports:[SidebarComponent],
+  declarations: [
+    SidebarComponent
+  ],
+  exports:[
+    SidebarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,        
+    AppRoutingModule
+
   ]
 })
 export class SharedModule { }
