c049a7a597b9178cd8016a4cd4ed28c42e49ecd7
coazit <coazit@gmail.com>
2021-03-01 16:54:10 -0500

    SwitchMap rxjs/operators

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
 import { Country } from '../../interfaces/pais.interface';
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-ver-pais',
@@ -19,16 +20,26 @@ export class VerPaisComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe(
-      ({ id }) => {
-        console.log(id);
-        this.paisService.getPaisPorAlpha(id).subscribe(
-          pais => {
-            console.log(pais);
-            this.pais = pais;
-          }
-        )
-      })    
+    this.activateRoute.params
+    .pipe(
+      switchMap(({id}) => this.paisService.getPaisPorAlpha(id))
+    )
+    .subscribe(
+      resp => {
+        console.log(resp);
+      }      
+    )
+
+    // this.activateRoute.params.subscribe(
+    //   ({ id }) => {
+    //     console.log(id);
+    //     this.paisService.getPaisPorAlpha(id).subscribe(
+    //       pais => {
+    //         console.log(pais);
+    //         this.pais = pais;
+    //       }
+    //     )
+    //   })    
   }
 
 }
